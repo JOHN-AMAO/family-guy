42288ac814f5b37df581341670f6d7de766fdeac
JOHN-AMAO <amaojohn767@gmail.com>
2023-12-18 01:07:06 +0100

    api recreated

--- app/api/characters/route.ts
+++ app/api/characters/route.ts
@@ -2,5 +2,5 @@ import characters from "@/data/characters.json";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ characters: characters.data });
+  return NextResponse.json({ character: characters.data });
 }
